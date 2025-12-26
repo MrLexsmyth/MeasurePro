2ba1ca560f238635e0fad7015e283b5ad13a86cc
MrLexsmyth <shittuodunayo4@gmail.com>
2025-12-26 16:20:29 +0100

    update

--- server/index.js
+++ server/index.js
@@ -4,13 +4,17 @@ dotenv.config();
 import app from "./src/app.js";
 import { connectDB } from "./src/config/db.js";
 
+const PORT = process.env.PORT || 5000;
+
+// DB
 connectDB();
 
-app.listen(5000, () => {
-  console.log("Server running on port 5000");
-});
-
+// Root health check
 app.get("/", (req, res) => {
   res.send("MeasurePro API is running 🚀");
 });
 
+// Start server
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
